343226b16cecf9396a0d0ac26dba1ade0746183f
James DeVito <jamesdevito@james-devitos-macbook-pro.local>
2014-04-08 20:02:43 -0400

    prototype install fades

--- Arduino/prototype_install/prototype_install.ino
+++ Arduino/prototype_install/prototype_install.ino
@@ -3,6 +3,8 @@
 
 // How many leds in your strip?
 #define NUM_LEDS 10
+#define DATA_PIN 11
+#define CLOCK_PIN 13
 
 //Initialize the Mux Shield
 MuxShield muxShield;
@@ -19,6 +21,14 @@ int index[numChannels];                  // the index of the current reading
 int total[numChannels];                  // the running total
 int average[numChannels];                // the average
 
+int theThreshold = 10;
+int THRESH[numChannels] = {
+  5,2,9,9,10,15,15,13,15,17 };                // the threshold--- animate to white if dropping below
+
+int frameCount[numChannels];
+boolean isAnimating[numChannels];
+int maxFrames = 250;
+
 // the setup routine runs once when you press reset:
 void setup() {
   FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
@@ -29,62 +39,94 @@ void setup() {
     for (int thisReading = 0; thisReading < numReadings; thisReading++){
       readings[thisVal][thisReading] = 0;        
     }
+    //    THRESH[thisVal] = theThreshold;
+    frameCount[thisVal] = 0;
+    isAnimating[thisVal] = false;
   }
 
-   //Set I/O 1, I/O 2, and I/O 3 as analog inputs
-    muxShield.setMode(1,ANALOG_IN);
-//muxShield.setMode(2,ANALOG_IN);
-//muxShield.setMode(3,ANALOG_IN);
+
+  //Set I/O 1, I/O 2, and I/O 3 as analog inputs
+  muxShield.setMode(1,ANALOG_IN);
+  //muxShield.setMode(2,ANALOG_IN);
+  //muxShield.setMode(3,ANALOG_IN);
 
 }
 
-//Arrays to store analog values after recieving them
-//int IO1AnalogVals[16];
-// int IO2AnalogVals[16];
-// int IO3AnalogVals[16];
 
 int chan = 0;
 
 // the loop routine runs over and over again forever:
 void loop() {
-  
-  
 
-//  Serial.println("START");
   // read the input on analog pin 0:
   for(chan = 0; chan < numChannels; chan++){
-    leds[chan] =CRGB::Red;
     // subtract the last reading:
-  total[chan]= total[chan] - readings[chan][index[chan]];       
-//  Serial.println("test");
-  // read from the sensor:  
-  if(chan < 16){
-  readings[chan][index[chan]] = muxShield.analogReadMS(1,chan);//analogRead(inputPin[chan]);
-  }
-//  else if(chan < 32){
-//  readings[chan][index[chan]] = muxShield.analogReadMS(2,chan-16);//analogRead(inputPin[chan]);
-//  }
-//  else if(chan < 48){
-//  readings[chan][index[chan]] = muxShield.analogReadMS(3,chan-32);//analogRead(inputPin[chan]);
-//  }
-  // add the reading to the total:
-  total[chan]= total[chan] + readings[chan][index[chan]];       
-  // advance to the next position in the array:  
-  index[chan]++;                    
+    total[chan]= total[chan] - readings[chan][index[chan]];       
+    //  Serial.println("test");
+    // read from the sensor:  
+    if(chan < 16){
+      readings[chan][index[chan]] = muxShield.analogReadMS(1,chan);//analogRead(inputPin[chan]);
+    }
+    //  else if(chan < 32){
+    //  readings[chan][index[chan]] = muxShield.analogReadMS(2,chan-16);//analogRead(inputPin[chan]);
+    //  }
+    //  else if(chan < 48){
+    //  readings[chan][index[chan]] = muxShield.analogReadMS(3,chan-32);//analogRead(inputPin[chan]);
+    //  }
+    // add the reading to the total:
+    total[chan]= total[chan] + readings[chan][index[chan]];       
+    // advance to the next position in the array:  
+    index[chan]++;                    
 
-  // if we're at the end of the array...
-  if (index[chan] >= numReadings){           
-    index[chan] = 0;                           
-}
+    // if we're at the end of the array...
+    if (index[chan] >= numReadings){           
+      index[chan] = 0;                           
+    }
 
-  // calculate the average:
-  average[chan] = total[chan] / numReadings;         
-  // send it to the computer as ASCII digits
-  
+    // calculate the average:
+    average[chan] = total[chan] / numReadings;         
+    // send it to the computer as ASCII digits
+
+
+      Serial.print(average[chan]);
+    Serial.print("\t");
+
+
+
+
+
+    if((average[chan] < THRESH[chan]) && !isAnimating[chan]){
+      //START ANIMATION
+      frameCount[chan] = 0;
+      isAnimating[chan] = true;
+    }
+
+
+
+    if(isAnimating[chan] && (frameCount[chan] < maxFrames)){
+      leds[chan].r = 255;
+      leds[chan].g = map(frameCount[chan],0,maxFrames,0,255);
+      leds[chan].b = map(frameCount[chan],0,maxFrames,0,255);
+      frameCount[chan]++;  
+    }
+    else{
+      leds[chan].r = 255;
+      leds[chan].g =0;
+      leds[chan].b = 0; 
+    }
+    if((average[chan] < THRESH[chan]) && frameCount[chan] == maxFrames )  {
+      isAnimating[chan] = false; 
+    }
+
   }
-  
+
+
+  Serial.println();
   FastLED.show();
 
 }
+
+
+
 
 
